43513e5a9fff718164dda7b97fe7efce319a4ae4
Samir Chaudhary <samirchaudhary.cs23@bmsce.ac.in>
2026-02-25 17:48:32 +0000

    MORE CLEAR BACKGROUND

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,10 +8,10 @@ export default function AuthLayout({
   return (
     <main className="relative min-h-svh flex items-center justify-center p-4">
         <Image
-            src="https://images.unsplash.com/photo-1518770660439-4636190af475?q=80&w=2070&auto=format&fit=crop"
-            alt="Circuit board texture background"
+            src="https://images.unsplash.com/photo-1535223289827-42f1e9919769?q=80&w=2070&auto=format&fit=crop"
+            alt="Abstract technology background"
             fill
-            className="object-cover -z-10 filter blur-md brightness-[.4]"
+            className="object-cover -z-10 filter blur-md brightness-50"
         />
         {children}
     </main>
